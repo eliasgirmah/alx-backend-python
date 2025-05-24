4956252f45c324e2e403a8af3019978a50ba21f7
Elias <eliasgirma.h@gmail.com>
2025-05-24 15:18:27 +0300

    excute and concurrent

---
+++ python-context-async-perations-0x02/3-concurrent.py
@@ -0,0 +1,34 @@
+# 2-concurrent.py
+
+import asyncio
+import aiosqlite
+
+DB_NAME = "my_database.sqlite3"
+
+async def async_fetch_users():
+    async with aiosqlite.connect(DB_NAME) as db:
+        async with db.execute("SELECT * FROM users") as cursor:
+            rows = await cursor.fetchall()
+            print("All users:")
+            for row in rows:
+                print(row)
+            return rows
+
+async def async_fetch_older_users():
+    async with aiosqlite.connect(DB_NAME) as db:
+        async with db.execute("SELECT * FROM users WHERE age > 40") as cursor:
+            rows = await cursor.fetchall()
+            print("\nUsers older than 40:")
+            for row in rows:
+                print(row)
+            return rows
+
+async def fetch_concurrently():
+    await asyncio.gather(
+        async_fetch_users(),
+        async_fetch_older_users()
+    )
+
+# Run the coroutine
+if __name__ == "__main__":
+    asyncio.run(fetch_concurrently())
