0e70f9bb561b3b46e6dd6b3edc10f44806dc9aea
Elias <eliasgirma.h@gmail.com>
2025-05-31 16:24:10 +0300

    test task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -56,3 +56,23 @@ class TestGithubOrgClient(TestCase):
             self.assertEqual(result, ["repo1", "repo2", "repo3"])
             mock_url.assert_called_once()
             mock_get_json.assert_called_once_with("http://mocked.repos.url")
+
+
+     @parameterized.expand([
+        (
+            {"license": {"key": "my_license"}}, "my_license", True
+        ),
+        (
+            {"license": {"key": "other_license"}}, "my_license", False
+        ),
+        (
+            {"license": None}, "my_license", False
+        ),
+        (
+            {}, "my_license", False
+        ),
+    ])
+    def test_has_license(self, repo, license_key, expected):
+        """Test has_license returns correct boolean"""
+        result = GithubOrgClient.has_license(repo, license_key)
+        self.assertEqual(result, expected)
