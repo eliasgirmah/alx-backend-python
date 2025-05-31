91a479c8ae2006f6c9217f16edea2108eec63e93
Elias <eliasgirma.h@gmail.com>
2025-05-31 16:19:37 +0300

    test task 6

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -19,12 +19,10 @@ class TestGithubOrgClient(TestCase):
         """Test that GithubOrgClient.org returns correct result"""
         mock_get_json.return_value = expected
         client = GithubOrgClient(org_name)
-
         self.assertEqual(client.org, expected)
         mock_get_json.assert_called_once_with(
-    f"https://api.github.com/orgs/{org_name}"
-   )
-
+            f"https://api.github.com/orgs/{org_name}"
+        )
 
     def test_public_repos_url(self):
         """Test GithubOrgClient._public_repos_url returns expected value from mocked org"""
@@ -33,31 +31,25 @@ class TestGithubOrgClient(TestCase):
             new_callable=PropertyMock
         ) as mock_org:
             mock_org.return_value = {"repos_url": "http://mocked.url"}
-
             client = GithubOrgClient("test_org")
             self.assertEqual(client._public_repos_url, "http://mocked.url")
 
     @patch('client.get_json')
     def test_public_repos(self, mock_get_json):
         """Test GithubOrgClient.public_repos returns expected repo list"""
-        # Simulated JSON response from GitHub
         mock_payload = [
             {"name": "repo1"},
             {"name": "repo2"},
             {"name": "repo3"}
         ]
         mock_get_json.return_value = mock_payload
-
-        # Patch the _public_repos_url property to return a dummy URL
         with patch(
             'client.GithubOrgClient._public_repos_url',
             new_callable=PropertyMock
         ) as mock_url:
             mock_url.return_value = "http://mocked.repos.url"
-
             client = GithubOrgClient("test_org")
             result = client.public_repos()
-
             self.assertEqual(result, ["repo1", "repo2", "repo3"])
             mock_url.assert_called_once()
             mock_get_json.assert_called_once_with("http://mocked.repos.url")
