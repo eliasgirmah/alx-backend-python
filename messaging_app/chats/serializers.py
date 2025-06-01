3949123ecd95529ac4b7d8e3ea852f28fa5cb656
Elias <eliasgirma.h@gmail.com>
2025-06-01 06:36:05 +0300

    serializers

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -2,12 +2,18 @@ from rest_framework import serializers
 from .models import User, Conversation, Message
 
 class UserSerializer(serializers.ModelSerializer):
+    user_id = serializers.UUIDField(source='id', read_only=True)
+    phone_number = serializers.CharField(required=False, allow_blank=True)
+    first_name = serializers.CharField(required=False, allow_blank=True)
+    last_name = serializers.CharField(required=False, allow_blank=True)
     class Meta:
         model = User
         fields = ['user_id', 'username', 'email', 'phone_number', 'first_name', 'last_name']
         read_only_fields = ['user_id']
 
 class MessageSerializer(serializers.ModelSerializer):
+    message_id = serializers.UUIDField(source='id', read_only=True)
+    conversation = serializers.PrimaryKeyRelatedField(queryset=Conversation.objects.all())
     sender = UserSerializer(read_only=True)
 
     class Meta:
@@ -16,6 +22,8 @@ class MessageSerializer(serializers.ModelSerializer):
         read_only_fields = ['message_id', 'sent_at']
 
 class ConversationSerializer(serializers.ModelSerializer):
+    conversation_id = serializers.UUIDField(source='id', read_only=True)
+    
     participants = UserSerializer(many=True, read_only=True)
     sent_messages = MessageSerializer(many=True, read_only=True, source='messages')
 
