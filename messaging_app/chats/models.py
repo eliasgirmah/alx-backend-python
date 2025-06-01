130552a1f4ca997bb128a659a299156a34081391
Elias <eliasgirma.h@gmail.com>
2025-06-01 05:42:26 +0300

    models for users, messages, and conversations

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
 # Create your models here.
+class User(AbstractUser):
+    bio=models.TextField(max_length=500, blank=True, null=True)
+    
+    def __str__(self):
+        return self.username
+class conversation(models.Model):
+    participants= models.ManyToManyField(User, related_name="conversations")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"conversation {self.id}"
+    
+class message(models.Model):
+    conversation = models.ForeignKey(conversation, on_delete=models.CASCADE, related_name="sent_messages")
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messages")
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"From {self.sender.username}: {self.content[:20]}"
+    
+
+    
+
--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     'chats',
 ]
@@ -123,3 +124,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'chats.user'
