545c7f606bb333cd97ea22ce628c7e57f20db2b8
Elias <eliasgirma.h@gmail.com>
2025-06-08 13:58:07 +0300

    copy messaging app

--- Django-Middleware-0x03/messaging_app/chats/auth.py
+++
@@ -1,9 +0,0 @@
-# messaging_app/chats/auth.py
-
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-
-class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
-    def validate(self, attrs):
-        data = super().validate(attrs)
-        data['username'] = self.user.username
-        return data
